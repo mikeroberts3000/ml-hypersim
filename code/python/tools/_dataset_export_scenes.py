737a7bcd15c736a4a1f93f9d1ab5574f5b1bc318
Mike Roberts <mlr@apple.com>
2020-12-22 01:09:04 -0800

    added support for exporting scenes from 3ds Max

---
+++ code/python/tools/_dataset_export_scenes.py
@@ -0,0 +1,180 @@
+import inspect
+import itertools
+import fnmatch
+import os
+from pymxs import runtime as rt
+import shutil
+
+#
+# can't import files from current dir by default, so duplicate path_utils here
+#
+
+import os, sys, inspect
+
+def add_path_to_sys_path(path, mode, frame):
+    assert mode == "unchanged" or mode == "relative_to_cwd" or mode == "relative_to_current_source_dir"
+    if mode == "unchanged":
+        if path not in sys.path:
+            sys.path.insert(0,path)
+    if mode == "relative_to_cwd":
+        realpath = os.path.realpath(os.path.abspath(path))
+        if realpath not in sys.path:
+            sys.path.insert(0,realpath)
+    if mode == "relative_to_current_source_dir":
+        realpath = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(frame))[0],path)))
+        if realpath not in sys.path:
+            sys.path.insert(0,realpath)
+
+def get_current_source_file_path(frame):
+    return os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(frame))[0])))
+
+#
+# define some useful utility functions
+#
+
+import MaxPlus
+
+def _print(obj):
+    string = str(obj)
+    MaxPlus.Core.EvalMAXScript('logsystem.logEntry "' + string.replace('\\', '\\\\').replace('"', '\\"') + '" broadcast:true')
+
+def _eval(string, silent=False):
+    if not silent:
+        _print("[HYPERSIM: _DATASET_EXPORT_SCENES] Executing MAXScript: " + string)
+    return MaxPlus.Core.EvalMAXScript(string)
+
+
+
+#
+# parse command-line args
+#
+
+for k in rt.maxops.mxsCmdLineArgs.keys:
+    _print('rt.maxops.mxsCmdLineArgs["' + k + '"] = ' + rt.maxops.mxsCmdLineArgs[k])
+
+args_dataset_dir = rt.maxops.mxsCmdLineArgs[rt.name("dataset_dir")]
+
+if rt.name("scene_names") in rt.maxops.mxsCmdLineArgs.keys:
+    args_scene_names = rt.maxops.mxsCmdLineArgs[rt.name("scene_names")]
+else:
+    args_scene_names = None
+
+assert os.path.exists(args_dataset_dir)
+
+#
+# parse dataset config
+#
+
+add_path_to_sys_path(args_dataset_dir, mode="relative_to_cwd", frame=inspect.currentframe())
+import _dataset_config
+
+
+
+_print("")
+_print("")
+_print("")
+_print("[HYPERSIM: _DATASET_EXPORT_SCENES] Begin...")
+_print("")
+_print("")
+_print("")
+
+
+
+dataset_scenes_dir = os.path.join(args_dataset_dir, "scenes")
+
+if args_scene_names is not None:
+    scenes = [ s for s in _dataset_config.scenes if fnmatch.fnmatch(s["name"], args_scene_names) ]
+else:
+    scenes = _dataset_config.scenes
+
+
+
+#
+# disable VRay prompts
+#
+
+_eval('setVRaySilentMode()')
+
+#
+# export scenes
+#
+
+for s in scenes:
+
+    # generate file names
+    scene_name     = s["name"]
+    scene_max_file = s["asset_file"] + ".max"
+    scene_dir      = os.path.join(dataset_scenes_dir, scene_name)
+    max_dir        = os.path.join(scene_dir, "_asset")
+    max_export_dir = os.path.join(scene_dir, "_asset_export")
+
+    max_file                             = os.path.join(max_dir, scene_max_file)
+    metadata_cameras_max_export_csv_file = os.path.join(max_export_dir, "metadata_cameras_asset_export.csv")
+    obj_file                             = os.path.join(max_export_dir, "scene.obj")
+    vrscene_file                         = os.path.join(max_export_dir, "scene.vrscene")
+
+    # create output dirs
+    if not os.path.exists(max_export_dir): os.makedirs(max_export_dir)
+
+    # loadMaxFile
+    retval = _eval('loadMaxFile @"' + max_file + '" useFileUnits:true')
+    if not retval.Get():
+        _print("[HYPERSIM: _DATASET_EXPORT_SCENES] Failed to load " + max_file)
+        assert False
+
+    # export cameras
+    with open(metadata_cameras_max_export_csv_file, "w") as f_cameras:
+
+        f_cameras.write("camera_name\n")
+
+        for ci in range(len(rt.cameras)):
+        
+            camera = rt.cameras[ci]
+            if not "target" in camera.name.lower() and not "terget" in camera.name.lower():
+
+                camera_name = "cam_" + camera.name
+
+                _print("[HYPERSIM: _DATASET_EXPORT_SCENES] Exporting camera: " + camera_name)
+                f_cameras.write(camera_name + "\n")
+
+                camera_file = os.path.join(max_export_dir, camera_name + ".csv")
+
+                with open(camera_file, "w") as f_camera:
+
+                    f_camera.write(
+                        "rotation_world_from_obj_00,rotation_world_from_obj_01,rotation_world_from_obj_02," + \
+                        "rotation_world_from_obj_10,rotation_world_from_obj_11,rotation_world_from_obj_12," + \
+                        "rotation_world_from_obj_20,rotation_world_from_obj_21,rotation_world_from_obj_22," + \
+                        "translation_world_from_obj_x,translation_world_from_obj_y,translation_world_from_obj_z\n")
+
+                    for ti in range(rt.animationRange.start, rt.animationRange.end+1):
+
+                        # Note that we iterate in column-major order because 3ds Max returns the transpose of the R_world_from_cam matrix,
+                        # where R_world_from_cam satisfies the equation: p_world == R_world_from_cam*p_cam for the camera space point p_cam
+                        # and the world space point p_world.
+                        for c,r in itertools.product(range(1,4),range(1,4)):
+                            retval = _eval("at time " + str(ti) + " cameras[" + str(ci+1) + "].transform[" + str(r) + "][" + str(c) + "]", silent=True).Get()
+                            f_camera.write("%.20f,"%retval)
+                        for c in range(1,4):
+                            retval = _eval("at time " + str(ti) + " cameras[" + str(ci+1) + "].transform[" + str(4) + "][" + str(c) + "]", silent=True).Get()
+                            if c in range(1,3):
+                                sep = ","
+                            else:
+                                sep = "\n"
+                            f_camera.write("%.20f%s"%(retval,sep))
+
+    # exportFile
+    _eval('exportFile @"' + obj_file + '" #noprompt')
+
+    # vrayExportRTScene
+    _eval('vrayExportRTScene @"' + vrscene_file + '"')
+
+
+
+_print("")
+_print("")
+_print("")
+_print("[HYPERSIM: _DATASET_EXPORT_SCENES] Finished.")
+_print("")
+_print("")
+_print("")
---
+++ code/python/tools/dataset_export_scenes.py
@@ -0,0 +1,134 @@
+from pylab import *
+
+import argparse
+import distutils.dir_util
+import inspect
+import os
+import shlex
+import shutil
+import subprocess
+import sys
+
+import path_utils
+path_utils.add_path_to_sys_path("..", mode="relative_to_current_source_dir", frame=inspect.currentframe())
+import _system_config
+
+assert sys.platform == "win32"
+assert os.path.exists(_system_config.max_sceneassets_photometric_dir)
+assert os.path.exists(_system_config.max_plugcfg_dir)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset_dir", required=True)
+parser.add_argument("--scene_names")
+args = parser.parse_args()
+
+assert os.path.exists(args.dataset_dir)
+
+
+
+print("[HYPERSIM: DATASET_EXPORT_SCENES] Begin...")
+
+
+
+# set up OBJ export
+obj_settings_file        = os.path.join(_system_config.max_plugcfg_dir, _system_config.max_objexp_ini_file)
+obj_settings_backup_file = os.path.join(args.dataset_dir, "_tmp", "_" + _system_config.max_objexp_ini_file)
+
+if os.path.exists(obj_settings_file):
+    print("[HYPERSIM: DATASET_EXPORT_SCENES] Configuring " + obj_settings_file + ", saving a backup at " + obj_settings_backup_file + "...")
+    shutil.copy(obj_settings_file, obj_settings_backup_file)
+else:
+    print("[HYPERSIM: DATASET_EXPORT_SCENES] Creating " + obj_settings_file + "...")
+
+# it would be cleaner to do this with the ConfigParser library
+obj_settings_str = \
+"""[General]
+UseLogging=1
+Preset=<NONE>
+MapSearchPath=
+
+[Geometry]
+FlipZyAxis=0
+Shapes=0
+ExportHiddenObjects=0
+FaceType=0
+TextureCoords=1
+Normals=1
+SmoothingGroups=0
+ObjScale=1.000000
+
+[Material]
+UseMaterial=1
+CreateMatLibrary=1
+ForceBlackAmbient=0
+UseMapPath=0
+MapPath=./maps/
+AutoMapChannel=0
+MapChannel=1
+ExtendedParams=0
+ConvertBitmaps=0
+RenderProcedural=1
+ResizeMaps=0
+ResizeMaps2n=0
+MapFormat=3
+MapSizeX=512
+MapSizeY=512
+
+[Output]
+RelativeIndex=0
+Target=0
+Precision=20
+
+[Optimize]
+optVertex=1
+optNormals=1
+optTextureCoords=1
+"""
+
+with open(obj_settings_file, "w") as f:
+    f.write(obj_settings_str)
+
+
+
+# copy global asset dependencies to dataset dir
+in_max_sceneassets_photometric_dir  = _system_config.max_sceneassets_photometric_dir
+out_max_sceneassets_photometric_dir = os.path.join(args.dataset_dir, "_asset", "sceneassets", "photometric")
+distutils.dir_util.copy_tree(in_max_sceneassets_photometric_dir, out_max_sceneassets_photometric_dir) # can't use shutil.copytree because I want to overwrite files if they exist
+
+
+
+current_source_file_path = path_utils.get_current_source_file_path(frame=inspect.currentframe())
+dataset_dir              = os.path.abspath(args.dataset_dir)
+tmp_dir                  = os.path.join(dataset_dir, "_tmp")
+
+if not os.path.exists(tmp_dir): os.makedirs(tmp_dir)
+
+maxbatch_bin_str          = _system_config.max_maxbatch_bin
+_dataset_export_scenes_py = '"' + os.path.join(current_source_file_path, "_dataset_export_scenes.py") + '"'
+log_str                   = '-log "' + os.path.join(tmp_dir, "_dataset_export_scenes_log.txt") + '"'
+listener_log_str          = '-listenerlog "' + os.path.join(tmp_dir, "_dataset_export_scenes_listenerlog.txt") + '"'
+dataset_dir_str           = '-mxsString dataset_dir:@"' + dataset_dir + '"'
+
+if args.scene_names is not None:
+    scene_names_str = '-mxsString scene_names:@"' + args.scene_names + '"'
+else:
+    scene_names_str = ''
+
+cmd = \
+    maxbatch_bin_str + \
+    " " + _dataset_export_scenes_py + \
+    " -v 5"                         + \
+    " " + log_str                   + \
+    " " + listener_log_str          + \
+    " " + dataset_dir_str           + \
+    " " + scene_names_str
+print("")
+print(cmd)
+print("")
+
+retval = subprocess.run(shlex.split(cmd)) # do not use os.system(...) because it doesn't parse command-line arguments cleanly
+retval.check_returncode()
+
+
+
+print("[HYPERSIM: DATASET_EXPORT_SCENES] Finished.")
